6baa89c4b329236f29047d029f748dc940143d94
leandro-costa <leandro.costa@ifba.edu.br>
2023-05-13 09:37:01 -0300

    Atualizar o Lanchonete.java

--- posts/code/freetime/Lanchonete.java
+++ posts/code/freetime/Lanchonete.java
@@ -6,9 +6,11 @@ public class Lanchonete {
 
     private ArrayList<Item> itens;
     private ArrayList<Pedido> pedidos;
+    
 
     public Lanchonete(){
         itens = new ArrayList<>();
+        pedidos = new ArrayList<>();
     }
 
     public void addItem(Item item) {
@@ -21,6 +23,13 @@ public class Lanchonete {
 
     public ArrayList<Item> retirarLanche(QRCode comprovante) {
         return comprovante.getPedido().getItens();
+    }
+
+    public Pedido criarNovoPedido(Cliente cliente){
+        Pedido pedido = new Pedido(pedidos.size(),cliente);
+        pedidos.add(pedido);
+        cliente.addPedido(pedido);
+        returno pedido;
     }
 
 }
